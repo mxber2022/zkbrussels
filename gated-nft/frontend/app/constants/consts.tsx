80cb310750620650c091c849c220e2bb18936a2d
mxber2022 <mxber2022@gmail.com>
2024-07-08 16:32:43 +0200

    update

---
+++ gated-nft/frontend/app/constants/consts.tsx
@@ -0,0 +1,11 @@
+export const GREETER_CONTRACT_ABI =
+  require("../../../zksync/artifacts-zk/contracts/Greeter.sol/Greeter.json").abi;
+export const NFT_CONTRACT_ABI =
+  require("../../../zksync/artifacts-zk/contracts/ERC721.sol/InfinityStones.json").abi;
+export const NETWORK_NAME = "zkSync Era Sepolia Testnet";
+export const NETWORK_ID = "0x12c";
+export const GREETER_ADDRESS = "0x401BBd241aE0c107844C70bD60608993d12571B6";
+export const NFT_CONTRACT_ADDRESS =
+  "0x2AB2CF61C56482e4c530B9488ba6e0b4909378Ca";
+export const PAYMASTER_CONTRACT_ADDRESS =
+  "0x22c41F8cf4CD666A1876a7A118fE456f0F49Eb4a";
